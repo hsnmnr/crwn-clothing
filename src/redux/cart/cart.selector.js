504f1df8dab9f8e3376fa5bdb873ed9eade619f6
mehmc <mehassanmunirch@gmail.com>
2020-01-29 03:42:51 +0500

    added cart-selector in redux/cart using reselect to replace cart-selectors with memorized selectors. Hence don't render on every unnecessary state change

---
+++ src/redux/cart/cart.selector.js
@@ -0,0 +1,18 @@
+import { createSelector } from "reselect";
+
+const selectCart = state => state.cart; // input selector
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
+);
