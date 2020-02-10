389ee23360043ddea1506d452ab52013a0a62f3b
mehmc <mehassanmunirch@gmail.com>
2020-02-11 00:00:12 +0500

    added react-stripe-checkout library of stripe payment, used it in stripe-button component. Also updated favicon and title in index.html

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import {
   selectCartItems,
   selectCartTotal
@@ -34,6 +35,17 @@ const CheckoutPage = ({ cartItems, total }) => (
     ))}
 
     <div className="total">TOTAL: ${total}</div>
+
+    <div className="test-warning">
+      <br />
+      *Use Following Credit Card For Test Payments*
+      <br />
+      4242 4242 4242 4242 - EXP: Any Future Date - CVV: 123
+      <br />
+      <br />
+    </div>
+
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
